87aeadb8081a5edd69238f2f287931a1fcffdfdd
Oliwia Rutkowska <oliwia.r789@gmail.com>
2022-04-28 22:45:13 +0200

    Modifications nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import data from "../data/pages"
 import "../styles/nav.css"
+import { Link } from 'react-router-dom'
 
 
 let Nav = () => {
@@ -8,7 +9,7 @@ let Nav = () => {
         <nav className="sidebar">
             <h1>REPOZEN</h1>
             <div>
-                {data.map(element => <span>{element.name}</span>)}
+                {data.map(element => <Link to={"/pages"+element.link}><div><span>{element.icon}</span><span>{element.name}</span></div></Link>)}
             </div>
         </nav>
     )
